e9dd951df2aff6e360185c8a2c4db5ae78e7b0a0
KatieBudgetExpress <katie@budget-express.com>
2025-09-30 12:58:32 -0400

    Create banque.controller.js

---
+++ src/modules/banques/banque.controller.js
@@ -0,0 +1,77 @@
+const { Bank } = require("../../models");
+const HttpError = require("../../utils/httpError");
+
+async function listBanks(req, res) {
+  const banks = await Bank.findAll({
+    order: [["createdAt", "DESC"]]
+  });
+
+  res.json({ data: banks });
+}
+
+async function getBank(req, res) {
+  const { id } = req.params;
+  const bank = await Bank.findByPk(id);
+
+  if (!bank) {
+    throw new HttpError(404, "Bank not found");
+  }
+
+  res.json({ data: bank });
+}
+
+async function createBank(req, res) {
+  const { name, institution, accountNumber, balance, description } = req.body;
+  const bank = await Bank.create({
+    name,
+    institution,
+    accountNumber,
+    balance,
+    description
+  });
+
+  res.status(201).json({ data: bank });
+}
+
+async function updateBank(req, res) {
+  const { id } = req.params;
+  const bank = await Bank.findByPk(id);
+
+  if (!bank) {
+    throw new HttpError(404, "Bank not found");
+  }
+
+  const { name, institution, accountNumber, balance, description } = req.body;
+
+  const updateData = {};
+  if (name !== undefined) updateData.name = name;
+  if (institution !== undefined) updateData.institution = institution;
+  if (accountNumber !== undefined) updateData.accountNumber = accountNumber;
+  if (balance !== undefined) updateData.balance = balance;
+  if (description !== undefined) updateData.description = description;
+
+  await bank.update(updateData);
+
+  res.json({ data: bank });
+}
+
+async function deleteBank(req, res) {
+  const { id } = req.params;
+  const bank = await Bank.findByPk(id);
+
+  if (!bank) {
+    throw new HttpError(404, "Bank not found");
+  }
+
+  await bank.destroy();
+
+  res.json({ data: null, message: "Bank deleted" });
+}
+
+module.exports = {
+  listBanks,
+  getBank,
+  createBank,
+  updateBank,
+  deleteBank
+};
