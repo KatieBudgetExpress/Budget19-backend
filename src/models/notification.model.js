49e0402192369c78b6fe86b27cf970633386efc6
KatieBudgetExpress <katie@budget-express.com>
2025-10-03 15:39:20 -0400

    Create notification.model.js

---
+++ src/models/notification.model.js
@@ -0,0 +1,24 @@
+// src/modules/notifications/notification.model.js
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../../config/database");
+
+const Notification = sequelize.define("Notification", {
+  message: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.ENUM("info", "warning", "error"),
+    allowNull: false,
+    defaultValue: "info",
+  },
+  isRead: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+}, {
+  tableName: "notifications",
+  timestamps: true, // createdAt, updatedAt
+});
+
+module.exports = Notification;
